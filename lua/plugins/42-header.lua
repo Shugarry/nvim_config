fe9bd20f28a8a1ce0f7e704743fa533275a41c5a
frey-gal <frey-gal@student.42barcelona.com>
2025-03-05 17:08:36 +0100

    added 42 header support

---
+++ lua/plugins/42-header.lua
@@ -0,0 +1,21 @@
+return {
+	"Diogo-ss/42-header.nvim",
+	cmd = { "Stdheader" },
+	keys = { "<F1>" },
+	opts = {
+		default_map = true, -- Default mapping <F1> in normal mode.
+		auto_update = true, -- Update header when saving.
+		user = "frey-gal",
+		---Default user.email.
+		mail = "frey-gal@student.42barcelona.com",
+		git = {
+			enabled = false,
+			bin = "git",
+			user_global = true,
+			email_global = true,
+		}
+	},
+	config = function(_, opts)
+		require("42header").setup(opts)
+	end
+}
