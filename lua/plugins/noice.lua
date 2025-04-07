9ef420a4f11c5909f8fca06df1498acb8ddf4e29
felix <felix.rey.galli@gmail.com>
2025-04-07 17:26:33 +0200

    very minor changes

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -11,13 +11,31 @@ return {
 		require("noice").setup({
 			views = {
 				cmdline_popup = {
-					border = {
-						style = "none",
-						padding = { 2, 3 },
+					position = {
+						row = 5,
+						col = "50%",
 					},
-					filter_options = {},
+					size = {
+						width = 60,
+						height = "auto",
+					},
+				},
+				popupmenu = {
+					relative = "editor",
+					position = {
+						row = 8,
+						col = "50%",
+					},
+					size = {
+						width = 60,
+						height = 10,
+					},
+					border = {
+						style = "rounded",
+						padding = { 0, 1 },
+					},
 					win_options = {
-						winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+						winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
 					},
 				},
 			},
@@ -34,12 +52,6 @@ return {
 				long_message_to_split = true,
 				inc_rename = false,
 				lsp_doc_border = false,
-			},
-			routes = {
-				{
-					view = "notify",
-					filter = { event = "msg_showmode" },
-				},
 			},
 		})
 	end
